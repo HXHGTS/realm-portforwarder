60e4262f095f1968da929c30cec3525ae8bce59b
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-07-06 10:38:51 +0800

    Update portforwarder.c

--- portforwarder.c
+++ portforwarder.c
@@ -9,12 +9,8 @@ int main() {
 	int UserInterface();
 	if (run_mode == 1) {
 		printf("正在新建并打开realm规则. . .\n");
-		config = fopen("/usr/bin/realm.json","w");
-		fprintf(config,"{\n    \"listening_addresses\": [\"0.0.0.0\"],\n    \"listening_ports\": [\"10000-10001\"],\n    \"remote_addresses\": [\"8.8.8.8\", \"8.8.4.4\"],\n    \"remote_ports\": [\"443\", \"853\"]\n}\n\n");
-		fclose(config);
-		service = fopen("/etc/systemd/system/realm.service", "w");
-		fprintf(service, "[Unit]\nDescription=realm\nAfter=network-online.target\nWants=network-online.target systemd-networkd-wait-online.service\n\n[Service]\nType=simple\nUser=root\nRestart=on-failure\nRestartSec=5s\nDynamicUser=true\nExecStart=/usr/bin/realm -c /usr/bin/realm.json\n\n[Install]\nWantedBy=multi-user.target\n\n");
-		fclose(service);
+		system("curl https://raw.githubusercontent.com/HXHGTS/realm-portforwarder/main/realm.json > /usr/bin/realm.json");
+		system("curl https://raw.githubusercontent.com/HXHGTS/realm-portforwarder/main/realm.service > /etc/systemd/system/realm.service");
 		system("vi /usr/bin/realm.json");
 		printf("正在下载realm. . .\n");
 		system("cd /usr/bin && wget https://github.com/zhboner/realm/releases/latest/download/realm -O realm && chmod +x realm");
